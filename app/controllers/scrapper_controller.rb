1cbe32a31c3f4b6c4ed5456bdc95f85fdadb9144
Dvid Silva <dsilva@doctorondemand.com>
2014-11-04 23:56:22 -0800

    instantiating a movie and storing in the database

--- app/controllers/scrapper_controller.rb
+++ app/controllers/scrapper_controller.rb
@@ -4,8 +4,24 @@ class ScrapperController < ApplicationController
 
     @response = HTTParty.get(URI.encode(url))
 
-    render text: @response.body
+    @movie = Movie.new
+    @result = JSON.parse(@response.body)
+    if @result["Title"] == nil
+      return render json: {:error => "COULDNT FIND THAT MOVIE :( "}.to_json
+    end
+    if( Movie.exists?(title: @result["Title"]) and Movie.exists?(year: @result["Year"] ))
+      return render json: {:error => "THAT MOVIE IS ALREADY IN THE DB :( "}.to_json
+    end
+    @movie.title = @result["Title"]
+    @movie.director = @result["Director"]
+    @movie.year = @result["Year"]
+    @movie.plot = @result["Plot"]
 
 
+    if @movie.save
+      render json: @movie
+    else
+      render json: {:error => "MEGAFAIL!"}.to_json
+    end
   end
 end
